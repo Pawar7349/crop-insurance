fc3cf0ee8e699a82acee8a171e69f3d346b21ee6
Pratik Pawar <pawarpratik7349@gmail.com>
2026-03-18 04:12:13 +0000

    add core contract functions

--- src/CropInsurance.sol
+++ src/CropInsurance.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 contract CropInsurance {
@@ -8,7 +8,7 @@ contract CropInsurance {
     ACTIVE,
     CLAIMED,
     EXPIRED
-  }
+  } 
 
   struct Policy {
     address farmer;         
@@ -38,4 +38,56 @@ contract CropInsurance {
     require(policies[_farmer].status == PolicyStatus.ACTIVE, "No active policy");
     _;
   }
+
+  event PolicyCreated(address indexed farmer, string cropType, uint256 coverageAmount);
+  event ClaimPaid(address indexed farmer, uint amount );
+  event PolicyExpired(address indexed farmer);
+
+  function registerPolicy(string memory _cropType, uint256 _landArea) external payable{
+    require(msg.value > 0,  "send eth to buy policy");
+    require(policies[msg.sender].status == PolicyStatus.INACTIVE || policies[msg.sender].farmer == address(0), "Policy already exiests");
+
+    uint256 premium = msg.value;
+    uint256 coverage = premium * 10;
+
+    policies[msg.sender] = Policy({
+      farmer:msg.sender,
+      cropType: _cropType,
+      landArea:_landArea,
+      premiumPaid:premium,
+      coverageAmount: coverage,
+      startDate: block.timestamp,
+      endDate:block.timestamp + 180 days,
+      status: PolicyStatus.INACTIVE
+    });
+
+    emit PolicyCreated(msg.sender, _cropType, coverage);
+
+  }
+
+  function activatePolicy(address _farmer) external onlyOwner {
+    require(policies[_farmer].status == PolicyStatus.INACTIVE, "Policy not inactive");
+    policies[_farmer].status = PolicyStatus.ACTIVE;
+  }
+
+  function processClaim (address _farmer) external onlyOwner hasActivePolicy(_farmer) {
+    uint256 payout = policies[_farmer].coverageAmount;
+    payable(_farmer).transfer(payout);
+    policies[_farmer].status = PolicyStatus.CLAIMED;
+
+    emit ClaimPaid(_farmer, payout);
+
+  }
+
+  function expirePolicy (address _farmer) external hasActivePolicy(_farmer) {
+    require(block.timestamp > policies[_farmer].endDate, "Policy not expired yet");
+    policies[_farmer].status = PolicyStatus.EXPIRED;
+
+    emit PolicyExpired(_farmer);
+  }
+
+  function getPolicy(address _farmer) external view returns(Policy memory){
+    return policies[_farmer];
+  }
+
 }
